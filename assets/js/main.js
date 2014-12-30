2d0634d8d55055583b349cc68db91ad78503a1a0
shubhamd <shubham.desale@gmail.com>
2014-12-30 20:52:35 +0530

    login form left

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+$(function(){	
+
+        var $window = $(window);
+	var scrollTime = 1.2;
+	var scrollDistance = 170;
+
+	$window.on("mousewheel DOMMouseScroll", function(event){
+
+		event.preventDefault();	
+
+		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+		var scrollTop = $window.scrollTop();
+		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+		TweenMax.to($window, scrollTime, {
+			scrollTo : { y: finalScroll, autoKill:true },
+				ease: Power1.easeOut,
+				overwrite: 5							
+			});
+
+	});
+});
+$('.navbar').scrollupbar();
+    $(document).ready(function(){
+   // cache the window object
+   $window = $(window);
+ 
+   		$('section[data-type="background"]').each(function(){
+     // declare the variable to affect the defined data-type
+	   		var $scroll = $(this);
+	                     
+	      	$(window).scroll(function() {
+	        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+	        var coords = '70% '+ yPos + 'px';
+	        $scroll.css({ backgroundPosition: coords });    
+      		}); 
+   		}); 
+	});
+	$(document).ready(function(){
+   // cache the window object
+   $window = $(window);
+ 
+   		$('section[data-type="background1"]').each(function(){
+     // declare the variable to affect the defined data-type
+	   		var $scroll = $(this);
+	                     
+	      	$(window).scroll(function() {
+	        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+	        yPos+=100;
+	        var coords = '70% '+ yPos + 'px';
+	        $scroll.css({ backgroundPosition: coords });    
+      		}); 
+   		}); 
+	});
